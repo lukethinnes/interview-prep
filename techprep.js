e82f1da32cca1cff73cac663e742090dad1cef7b
Luke Thinnes <lukethinnes@yahoo.com>
2021-10-18 10:23:53 -0600

    Added a frequency counter algorithm to study.

--- techprep.js
+++ techprep.js
@@ -410,3 +410,28 @@ function missingWords(str1, str2) {
   }
   return output
 }
+
+// A frequency counter pattern checking if two array contain the same values, only squared in the second array.
+
+function same(arr1, arr2) {
+  if(arr1.length!== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  for (let key in frequencyCounter1) {
+    if(!(key ** 2 in frequencyCounter2)) {
+      return false
+    }
+    if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return fales
+    }
+  }
+  return true
+}

3080d371c5775ab413d4929d5b2492ea0878a705
Luke Thinnes <lukethinnes@yahoo.com>
2021-10-24 21:03:51 -0600

    Included a pure recursion refactor.

--- techprep.js
+++ techprep.js
@@ -542,3 +542,19 @@ function collectOddValues(arr) {
   helper(arr)
   return result
 }
+
+// Refactored for "pure recursion"...
+
+function collectOddValues(arr) {
+  let newArr = []
+
+  if (arr.length === 0) {
+    return newArr
+  }
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0])
+  }
+  newArr = newArr.concat(collectOddValues(arr.slice(1)))
+  return newArr
+}
+

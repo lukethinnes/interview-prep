01cf78f4c06409416f9bb9d17b99fddeccea388c
Luke Thinnes <lukethinnes@yahoo.com>
2021-10-24 20:57:06 -0600

    Added a recursive function with a helper method to study.

--- techprep.js
+++ techprep.js
@@ -524,3 +524,21 @@ function fibseq(int) {
         return 1
     }
 } 
+
+// Using a helper method with recursion to collect all odd values from an array.
+
+function collectOddValues(arr) {
+  let result = []
+
+  function helper(helperInput) {
+    if(helperInput.length === 0) {
+      return
+    }
+    if (helperInput[0] % 2 !==0) {
+      result.push(helperInput[0])
+    }
+    helper(helperInput.slice(1))
+  }
+  helper(arr)
+  return result
+}
